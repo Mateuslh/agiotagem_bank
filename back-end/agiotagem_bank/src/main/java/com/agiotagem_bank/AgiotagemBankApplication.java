8429d9f3d6bb62f7507e7750f4b152d4cc9bb669
Mateuslh <91639503+Mateuslh@users.noreply.github.com>
2024-06-12 21:44:04 -0300

    [feat] srtat aplicação spring
    #2

---
+++ back-end/agiotagem_bank/src/main/java/com/agiotagem_bank/AgiotagemBankApplication.java
@@ -0,0 +1,13 @@
+package com.agiotagem_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiotagemBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgiotagemBankApplication.class, args);
+	}
+
+}
---
+++ back-end/agiotagem_bank/src/test/java/com/agiotagem_bank/AgiotagemBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agiotagem_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgiotagemBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
