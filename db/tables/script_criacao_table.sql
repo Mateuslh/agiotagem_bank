04c89742c87d1f4c6605c5ee4af473c8189b54ca
Mateuslh <91639503+Mateuslh@users.noreply.github.com>
2024-06-16 22:41:28 -0300

    [refatoring][fix][ #3 , #4 ] : Alterado tipo de dados, inserido relações na tabela de emprestimo.
    - Alterado para que o id da propria tabela seja referenciada apenas por "id" e não por "entidade_id".
    - Alterados as datas para receberem TIMESTAMP(6) ao inves de DATE.
    - Alterados para que as colunas de ID recebem BIGSERIAL.
    - Inserida a referencia na tabela emprestimo, para que a mesma referencie a conta de destino ticket #3

--- db/tables/script_criacao_table.sql
+++ db/tables/script_criacao_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE enderecos (
-    endereco_id SERIAL PRIMARY KEY,
-    rua VARCHAR(100),
+    id BIGSERIAL PRIMARY KEY,
+    logradouro VARCHAR(100),
     numero VARCHAR(10),
     cidade VARCHAR(50),
     estado VARCHAR(50),
@@ -8,99 +8,101 @@ CREATE TABLE enderecos (
 );
 
 CREATE TABLE usuarios (
-    usuario_id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL,
-    data_nascimento DATE,
+    data_nascimento TIMESTAMP(6),
     telefone VARCHAR(15),
-    endereco_id INT,
-    FOREIGN KEY (endereco_id) REFERENCES enderecos(endereco_id)
+    endereco_id BIGSERIAL,
+    FOREIGN KEY (endereco_id) REFERENCES enderecos(id)
 );
 
 CREATE TABLE agencias (
-    agencia_id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     nome_agencia VARCHAR(100) NOT NULL,
-    endereco_id INT,
+    endereco_id BIGSERIAL,
     telefone VARCHAR(15),
-    FOREIGN KEY (endereco_id) REFERENCES enderecos(endereco_id)
+    FOREIGN KEY (endereco_id) REFERENCES enderecos(id)
 );
 
 CREATE TABLE contas (
-    conta_id SERIAL PRIMARY KEY,
-    usuario_id INT NOT NULL,
-    agencia_id INT NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    usuario_id BIGSERIAL NOT NULL,
+    agencia_id BIGSERIAL NOT NULL,
     tipo_conta VARCHAR(50),
     saldo NUMERIC(15, 2) DEFAULT 0.00,
-    data_abertura DATE,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
-    FOREIGN KEY (agencia_id) REFERENCES agencias(agencia_id)
+    data_abertura TIMESTAMP(6),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (agencia_id) REFERENCES agencias(id)
 );
 
 CREATE TABLE tipos_transacoes (
-    tipo_transacao_id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     descricao VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE transacoes (
-    transacao_id SERIAL PRIMARY KEY,
-    conta_origem_id INT NOT NULL,
-    conta_destino_id INT NOT NULL,
-    tipo_transacao_id INT NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    conta_origem_id BIGSERIAL NOT NULL,
+    conta_destino_id BIGSERIAL NOT NULL,
+    tipo_transacao_id BIGSERIAL NOT NULL,
     valor NUMERIC(15, 2),
     data_transacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     descricao TEXT,
-    FOREIGN KEY (conta_origem_id) REFERENCES contas(conta_id),
-    FOREIGN KEY (conta_destino_id) REFERENCES contas(conta_id),
-    FOREIGN KEY (tipo_transacao_id) REFERENCES tipos_transacoes(tipo_transacao_id)
+    FOREIGN KEY (conta_origem_id) REFERENCES contas(id),
+    FOREIGN KEY (conta_destino_id) REFERENCES contas(id),
+    FOREIGN KEY (tipo_transacao_id) REFERENCES tipos_transacoes(id)
 );
 
 CREATE TABLE cartoes (
-    cartao_id SERIAL PRIMARY KEY,
-    conta_id INT NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    conta_id BIGSERIAL NOT NULL,
     numero_cartao VARCHAR(16) UNIQUE NOT NULL,
-    validade DATE,
+    validade TIMESTAMP(6),
     codigo_seguranca VARCHAR(4),
     limite NUMERIC(15, 2),
     tipo_cartao VARCHAR(50),
-    FOREIGN KEY (conta_id) REFERENCES contas(conta_id)
+    FOREIGN KEY (conta_id) REFERENCES contas(id)
 );
 
 CREATE TABLE emprestimos (
-    emprestimo_id SERIAL PRIMARY KEY,
-    usuario_id INT NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    usuario_id BIGSERIAL NOT NULL,
+    conta_destino_id BIGSERIAL NOT NULL,
     valor_emprestimo NUMERIC(15, 2),
     taxa_juros NUMERIC(5, 2),
-    data_contratacao DATE,
-    data_vencimento DATE,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id)
+    data_contratacao TIMESTAMP(6),
+    data_vencimento TIMESTAMP(6),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (conta_destino_id) REFERENCES contas(id)
 );
 
 CREATE TABLE pagamentos (
-    pagamento_id SERIAL PRIMARY KEY,
-    conta_id INT NOT NULL,
-    cartao_id INT,
+    id BIGSERIAL PRIMARY KEY,
+    conta_id BIGSERIAL NOT NULL,
+    cartao_id BIGSERIAL,
     valor NUMERIC(15, 2),
-    data_pagamento DATE,
+    data_pagamento TIMESTAMP(6),
     descricao TEXT,
-    FOREIGN KEY (conta_id) REFERENCES contas(conta_id),
-    FOREIGN KEY (cartao_id) REFERENCES cartoes(cartao_id)
+    FOREIGN KEY (conta_id) REFERENCES contas(id),
+    FOREIGN KEY (cartao_id) REFERENCES cartoes(id)
 );
 
 CREATE TABLE depositos (
-    deposito_id SERIAL PRIMARY KEY,
-    conta_id INT NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    conta_id BIGSERIAL NOT NULL,
     valor NUMERIC(15, 2),
-    data_deposito DATE,
+    data_deposito TIMESTAMP,
     descricao TEXT,
-    FOREIGN KEY (conta_id) REFERENCES contas(conta_id)
+    FOREIGN KEY (conta_id) REFERENCES contas(id)
 );
 
 CREATE TABLE historico_saldos (
-    historico_saldo_id SERIAL PRIMARY KEY,
-    conta_id INT NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    conta_id BIGSERIAL NOT NULL,
     saldo_anterior NUMERIC(15, 2),
     saldo_atual NUMERIC(15, 2),
     data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (conta_id) REFERENCES contas(conta_id)
+    FOREIGN KEY (conta_id) REFERENCES contas(id)
 );
