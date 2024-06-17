9e293b407201994898b7f43ffb03d6e23a774b94
Mateuslh <91639503+Mateuslh@users.noreply.github.com>
2024-06-16 23:38:00 -0300

    [fix]: Criado construtores para classe EntityId

--- back-end/agiotagem_bank/src/main/java/com/agiotagem_bank/model/EntityId.java
+++ back-end/agiotagem_bank/src/main/java/com/agiotagem_bank/model/EntityId.java
@@ -1,11 +1,15 @@
 package com.agiotagem_bank.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class EntityId {
 
