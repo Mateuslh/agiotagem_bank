524e1c06267a44b4f5084a00676020da7f536de0
Marcos Vinicius Goudinho da Silva <167702986+marcosgoudinho@users.noreply.github.com>
2024-06-25 00:12:37 -0300

    [FEAT]: Criado script para as 4 perguntas de negócio (2 scripts permanecem com erro).

--- db/functions/script_functions.sql
+++ db/functions/script_functions.sql
@@ -1,112 +1,102 @@
-DELIMITER //
+--Qual as 3 agencias que mais tiveram transações do tipo "deposito" no ano de 2024.--
+CREATE OR REPLACE FUNCTION public.top_agencias_depositos(ano integer)
+    RETURNS TABLE(agencia_id bigint, nome_agencia character varying, total_depositos bigint) 
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
 
-CREATE FUNCTION CartoesCreditoVencimento()
-RETURNS TABLE (
-    id BIGINT,
-    conta_id BIGINT,
-    numero_cartao VARCHAR(20),
-    tipo_cartao VARCHAR(50),
-    limite DECIMAL(15, 2),
-    validade DATE
-)
+AS $BODY$
 BEGIN
-    RETURN (
-        SELECT *
-        FROM cartoes
-        WHERE tipo_cartao = 'crédito'
-          AND limite = 6000
-          AND validade BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 30 DAY)
-    );
-END //
-
-DELIMITER ;
-GO 
-
-/*CALL CartoesCreditoVencimento();*/
-
-
-DELIMITER //
-
-CREATE PROCEDURE Top3AgenciasDepositos2024()
-BEGIN
+    RETURN QUERY
     SELECT 
-        a.id AS agencia_id, 
-        a.nome AS nome_agencia, 
-        COUNT(t.id) AS total_depositos
+        ag.id AS agencia_id, ag.nome_agencia, COUNT(d.id) AS total_depositos
 
-    FROM transacoes t
+    FROM agencias ag
 
-    JOIN agencias a ON t.agencia_id = a.id
+    JOIN contas c ON ag.id = c.agencia_id
 
-    JOIN tipos_transacoes tt ON t.tipo_transacao_id = tt.id
+    JOIN depositos d ON c.id = d.conta_id
 
-    WHERE tt.nome = 'deposito' AND YEAR(t.data_transacao) = 2024
+    WHERE EXTRACT(YEAR FROM d.data_deposito) = ano
 
-    GROUP BY a.id, a.nome
+    GROUP BY ag.id, ag.nome_agencia
 
-    ORDER BY total_depositos DESC
-    LIMIT 3;
-END //
+    ORDER BY total_depositos DESC LIMIT 3;
+END;
+$BODY$;
+--SELECT * FROM top_agencias_depositos(2024)--
 
-DELIMITER ;
-GO
+--Quais contas do tipo "corrente" possuem seu usuário com data de nascimento maior que "1990".--
+CREATE OR REPLACE FUNCTION public.contas_corrente_usuarios_nascimento(ano_nascimento date)
 
-/*CALL Top3AgenciasDepositos2024();*/
+    RETURNS TABLE(conta_id bigint, usuario_id bigint, nome_usuario character varying, data_nascimento date, tipo_conta character varying, saldo numeric) 
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
 
-
-DELIMITER //
-
-CREATE FUNCTION UsuariosSaldoNegativoAntes1990()
-RETURNS TABLE (
-    id BIGINT,
-    nome VARCHAR(100),
-    data_nascimento DATE,
-    saldo NUMERIC(15, 2)
-)
+AS $BODY$
 BEGIN
-    RETURN (
-        SELECT 
-            u.id,
-            u.nome,
-            u.data_nascimento,
-            c.saldo
-        FROM usuarios u
+    RETURN QUERY
+    SELECT c.id AS conta_id, u.id AS usuario_id, u.nome AS nome_usuario, u.data_nascimento, c.tipo_conta, c.saldo
 
-        JOIN contas c ON u.id = c.usuario_id
+    FROM contas c
 
-        JOIN agencias a ON c.agencia_id = a.id AND a.nome = 'Agência Central'
+    JOIN usuarios u ON c.usuario_id = u.id
 
-        JOIN historico_saldos hs ON c.id = hs.conta_id
-
-        WHERE u.data_nascimento < '1990-01-01'AND hs.saldo < 0  
-    );
-END //
-
-DELIMITER ;
-
-/*SELECT * FROM UsuariosSaldoNegativoAntes1990();*/
+    WHERE c.tipo_conta = 'Conta Corrente' AND u.data_nascimento > ano_nascimento;
+END;
+$BODY$;
+--SELECT * FROM public.contas_corrente_usuarios_nascimento('1990-01-01')--
 
 
-DELIMITER //
+--Quais usuários com cartão de tipo "crédito" possuem limite de R$6.000.--
+CREATE OR REPLACE FUNCTION public.usuarios_com_limite_especifico(
+	limite_cartao numeric)
+    RETURNS TABLE(id bigint) 
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
 
-CREATE PROCEDURE ContasCorrenteNascidosDepois1990()
+AS $BODY$
 BEGIN
-    SELECT 
-        c.id AS conta_id,
-        c.tipo_conta,
-        c.saldo,
-        u.id AS usuario_id,
-        u.nome AS usuario_nome,
-        u.data_nascimento
-    FROM 
-        contas c
-    JOIN 
-        usuarios u ON c.usuario_id = u.id
-    WHERE 
-        c.tipo_conta = 'corrente'
-        AND u.data_nascimento > '1990-12-31';  -- Considerando data de nascimento após 31 de dezembro de 1990
-END //
+    RETURN QUERY
+    SELECT u.id
+    FROM usuarios u
+    JOIN contas c ON u.id = c.usuario_id
+    JOIN cartoes ca ON c.id = ca.conta_id
+    WHERE ca.tipo_cartao = 'crédito'
+        AND ca.limite = limite_cartao;
+END;
+$BODY$;
+--SELECT * FROM public.usuarios_com_limite_especifico(6000.00)--
 
-DELIMITER ;
+--Quais usuários com data de nascimento antes do ano "1990" possuem saldo maior que 10000 na agencia "Agência Central".--
+CREATE OR REPLACE FUNCTION public.usuarios_saldo_agencia(ano_nascimento date, saldo_minimo numeric, nm_agencia character varying)
+    RETURNS TABLE(usuario_id bigint, nome_usuario character varying, email_usuario character varying, saldo_conta numeric) 
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
 
-/*CALL ContasCorrenteNascidosDepois1990();*/
+AS $BODY$
+BEGIN
+    RETURN QUERY
+    SELECT u.id AS usuario_id, u.nome AS nome_usuario, u.email AS email_usuario, c.saldo AS saldo_conta
+
+    FROM usuarios u
+
+    JOIN contas c ON u.id = c.usuario_id
+
+    JOIN agencias a ON c.agencia_id = a.id
+
+    WHERE u.data_nascimento < TO_DATE(ano_nascimento::TEXT, 'YYYY') AND c.saldo > saldo_minimo AND a.nome_agencia = nm_agencia;
+END;
+$BODY$;
+
+ALTER FUNCTION public.usuarios_saldo_agencia(date, numeric, character varying)
+    OWNER TO postgres;
+
+--SELECT * FROM usuarios_saldo_agencia(1990, 10000, 'Agência Central');--
